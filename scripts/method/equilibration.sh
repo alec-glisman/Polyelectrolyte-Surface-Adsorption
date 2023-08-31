67f071c83451bb0e9107c843d2e33c4ee953d9f7
Alec <30010253+alec-glisman@users.noreply.github.com>
2023-08-31 10:33:56 -0700

    nvt eqbm working stably

--- scripts/method/equilibration.sh
+++ scripts/method/equilibration.sh
@@ -20,12 +20,12 @@ project_path="${script_path}/../.."
 
 # Gromacs files
 mdp_path="${project_path}/parameters/mdp/molecular-dynamics"
-mdp_file_nvt="${project_path}/nvt_eqbm_10ns.mdp"
-mdp_file_npt="${project_path}/npt_eqbm_10ns.mdp"
+mdp_file_nvt="${mdp_path}/nvt_eqbm_10ns.mdp"
+mdp_file_npt="${mdp_path}/npt_eqbm_10ns.mdp"
 if [[ "${PRODUCTION_ENSEMBLE^^}" == "NVT" ]]; then
-    mdp_file_prod="${project_path}/nvt_eqbm_10ns.mdp"
+    mdp_file_prod="${mdp_path}/nvt_eqbm_10ns.mdp"
 elif [[ "${PRODUCTION_ENSEMBLE^^}" == "NPT" ]]; then
-    mdp_file_prod="${project_path}/npt_eqbm_10ns.mdp"
+    mdp_file_prod="${mdp_path}/npt_eqbm_10ns.mdp"
 else
     echo "ERROR: PRODUCTION_ENSEMBLE must be set to NVT or NPT"
     exit 1
@@ -45,7 +45,7 @@ echo "CRITICAL: Starting system equilibration"
 mkdir -p "${cwd}"
 cd "${cwd}" || exit
 
-# see if "2-output/system.gro" exists
+# check if "2-output/system.gro" exists
 if [[ -f "4-output/system.gro" ]]; then
     echo "WARNING: 4-output/system.gro already exists"
     echo "INFO: Exiting script"
@@ -56,20 +56,67 @@ fi
 # NVT equilibration #####################################################################
 # #######################################################################################
 echo "INFO: Starting NVT equilibration"
+previous_sim_name="em"
+sim_name="nvt_eqbm"
+
+# check if "1-nvt/nvt_eqbm.gro" exists
+if [[ -f "1-nvt/${sim_name}.gro" ]]; then
+    echo "WARNING: 1-nvt/nvt_eqbm.gro does not exist"
+    echo "INFO: Skipping NVT equilibration"
+else
+    {
+        # copy output files from initialization
+        prev_dir_base="../1-energy-minimization/2-output"
+        cp -p "${prev_dir_base}/system.gro" "${previous_sim_name}.gro" || exit 1
+        cp -p "${prev_dir_base}/topol.top" "topol.top" || exit 1
+        cp -p "${prev_dir_base}/index.ndx" "index.ndx" || exit 1
+
+        # replace temperature in mdp file
+        cp "${mdp_file_nvt}" "${sim_name}.mdp" || exit 1
+        sed -i "s/ref-t                     = 300/ref-t                     = ${TEMPERATURE_K}/g" "nvt_eqbm.mdp" || exit 1
+
+        # make tpr file for NVT equilibration
+        "${GMX_BIN}" -quiet -nocopyright grompp \
+            -f "${sim_name}.mdp" \
+            -c "${previous_sim_name}.gro" \
+            -p "topol.top" \
+            -o "${sim_name}.tpr" \
+            -maxwarn '1'
+
+        # run NVT equilibration
+        "${MPI_BIN}" -np '1' \
+            --map-by "ppr:1:node:PE=${CPU_THREADS}" \
+            --use-hwthread-cpus --bind-to 'hwthread' \
+            "${GMX_BIN}" -quiet -nocopyright mdrun -v \
+            -deffnm "${sim_name}" \
+            -pin on -pinoffset "${PIN_OFFSET}" -pinstride 1 -ntomp "${CPU_THREADS}" \
+            -gpu_id "${GPU_IDS}" || exit 1
+
+        # copy output files to 1-nvt
+        mkdir -p "1-nvt"
+        cp -p "${sim_name}."* -t "1-nvt/" || exit 1
+        rm "${sim_name}."* || exit 1
+        cp -p "1-nvt/${sim_name}.gro" "${sim_name}.gro" || exit 1
+    } >>"${log_file}" 2>&1
+fi
+
+# #######################################################################################
+# NPT equilibration #####################################################################
+# #######################################################################################
+echo "INFO: Starting NPT equilibration"
+previous_sim_name="${sim_name}"
+sim_name="npt_eqbm"
 
 # TODO: Use sed to replace the following variables in the mdp file:
 #       - TEMP
 #       - PRESSURE
 
 # #######################################################################################
-# NPT equilibration #####################################################################
-# #######################################################################################
-echo "INFO: Starting NPT equilibration"
-
-# #######################################################################################
 # Production equilibration ##############################################################
 # #######################################################################################
 echo "INFO: Starting production equilibration"
+previous_sim_name="${sim_name}"
+sim_name="prod_eqbm"
 
 # #######################################################################################
 # Clean up ##############################################################################
