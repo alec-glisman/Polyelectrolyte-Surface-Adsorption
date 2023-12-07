573bf675a64ac5d8827b8fce8fd897bf4808db72
Alec <30010253+alec-glisman@users.noreply.github.com>
2023-12-07 09:32:03 -0800

    Rewrote methods as class and updated for new simulations

--- python/probability_exchange.py
+++ python/probability_exchange.py
@@ -19,6 +19,226 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+# matplotlib settings
+plt.rcParams["axes.formatter.use_mathtext"] = True
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["font.serif"] = "cmr10"
+
+# global variables
+VERBOSE: bool = True
+
+
+class ParseGmxLog:
+    """
+    A class for parsing Gromacs log files and extracting exchange probabilities
+
+    Attributes:
+        f_log (Path): Path to the log file.
+        text (str): Content of the log file.
+        n_replica (int): Number of replicas in the simulation.
+        t_final (float): Final simulation time in ns.
+        df_repl_pr (pd.DataFrame): Exchange probabilities at each step.
+        df_repl_pr_summary (pd.DataFrame): Average exchange probabilities.
+    """
+
+    def __init__(self, f_log: Path):
+        """
+        Initialize the ParseGmxLog object.
+
+        Args:
+            f_log (Path): Path to the log file.
+
+        Raises:
+            TypeError: If f_log is not a Path object.
+            FileNotFoundError: If f_log does not exist or is a directory.
+            TypeError: If f_log is not a .log file.
+        """
+        if not isinstance(f_log, Path):
+            raise TypeError(f"Expected Path object, got {type(f_log).__name__}")
+        if not f_log.exists() or not f_log.is_file():
+            raise FileNotFoundError(f"File not found: {f_log}")
+        if not f_log.is_file():
+            raise FileNotFoundError(f"Expected file, got directory: {f_log}")
+        if f_log.suffix != ".log":
+            raise TypeError(f"Expected .log file, got {f_log.suffix}")
+
+        self.f_log = f_log
+        self.name = f_log.name.split(".log")[0]
+        self.text = f_log.read_text()
+
+        self.n_replica = self._parse_n_replica()
+        self.t_initial, self.t_final = self._parse_t_min_max()
+        self.df_repl_pr = self.exchange_probabilities()
+        self.df_repl_pr_summary = self.exchange_probabilities_summary()
+
+    def _parse_n_replica(self) -> int:
+        """
+        Parse the number of replicas from the log file.
+
+        The number of replicas is given in the log file in the form
+        Repl  There are 8 replicas:
+        We can extract the number of replicas by searching for the string
+        "There are" and then extracting the integer that follows.
+
+        Returns:
+            int: Number of replicas.
+        """
+        # search for string
+        n_replica = re.findall(r"(?<=Repl  There are )\d+", self.text)
+
+        # check that only one match was found
+        if len(n_replica) != 1:
+            raise ValueError(f"Expected 1 match, got {len(n_replica)}")
+
+        # convert to integer
+        n_replica = int(n_replica[0])
+
+        return n_replica
+
+    def _parse_t_min_max(self) -> tuple[float, float]:
+        """
+        Parse the final simulation time from the log file.
+
+        The final simulation time is given in the log file in the form
+        Replica exchange at step 17000 time 34.00000
+        We can extract the final simulation time by searching for the string
+        "time" and then extracting the float that follows.
+
+        Returns:
+            tuple: Minimum and maximum simulation times in ns.
+        """
+        times = re.findall(r"(?<=time\s)\d+.\d+", self.text)
+        times = [float(x) for x in times]
+
+        try:
+            tf = max(times) / 1000.0
+        except ValueError:
+            tf = np.nan
+
+        try:
+            t0 = min(times) / 1000.0
+        except ValueError:
+            t0 = np.nan
+
+        return t0, tf
+
+    def _parse_repl_pr(self, line: str) -> list[float]:
+        """
+        Parse exchange probabilities from a single line of text. Exchanges are only
+        attempted between neighboring replicas, so the exchange probabilities are
+        only given for the even or odd replicas. This function places np.nan values
+        between the exchange probabilities to account for this.
+
+        Output is a list of exchange probabilities in the form
+            P(0<->1), P(1<->2), ..., P(n-2<->n-1), P(n-1<->n)
+
+        Args:
+            line (str): Line of text containing exchange probabilities in the form
+                Repl pr        .52       .00       1.0
+            or
+                Repl pr   .00       .00       .00       .00
+
+        Returns:
+            list: List of exchange probabilities.
+        """
+        # remove alphabetic characters and leading/trailing whitespace
+        line = re.sub(r"[a-zA-Z]", "", line).strip()
+        # convert whitespace to comma
+        line = re.sub(r"\s+", ",", line)
+
+        # convert comma separated string to list of floats
+        lst_float = []
+        for val in line.split(","):
+            try:
+                lst_float.append(float(val))
+            except ValueError:
+                warnings.warn(f"ValueError: {val}")
+                lst_float.append(-1)
+
+        # place np.nan elements between list elements
+        lst_pad = [np.nan] * len(lst_float)
+        lst_complete = [x for pair in list(zip(lst_float, lst_pad)) for x in pair][:-1]
+
+        # prepend and append to list if in example 1 of Args
+        if len(lst_complete) != self.n_replica - 1:
+            lst_complete = [np.nan] + lst_complete + [np.nan]
+
+        # check that list is the same length as the number of replica exchanges
+        if len(lst_complete) != self.n_replica - 1:
+            raise ValueError(
+                f"Expected {self.n_replica} values, got {len(lst_complete)}"
+            )
+
+        return lst_complete
+
+    def exchange_probabilities(self) -> pd.DataFrame:
+        """
+        Parse exchange probabilities from the log file.
+
+        Exchange probabilities are given in the log file in the form
+        Repl pr        .52       .00       1.0
+        or
+        Repl pr   .00       .00       .00       .00
+        We can extract the exchange probabilities by searching for the string
+        "Repl pr" and then extracting the floats that follow.
+
+        Returns:
+            pd.DataFrame: Exchange probabilities at each step.
+        """
+        # search for string
+        prob_txt = re.findall(r"(?<=Repl pr).*", self.text)
+
+        # parse exchange probabilities from each line
+        prob_flt = []
+        for line in prob_txt:
+            prob_flt.append(self._parse_repl_pr(line))
+
+        # convert to pandas data frame
+        cols = [f"{i}-{i+1}" for i in range(self.n_replica - 1)]
+        return pd.DataFrame(prob_flt, columns=cols)
+
+    def exchange_probabilities_summary(self) -> pd.DataFrame:
+        """
+        Calculate the average exchange probability for each replica pair.
+
+        Returns:
+            pd.DataFrame: Average exchange probabilities.
+        """
+        if self.df_repl_pr is None:
+            self.df_repl_pr = self.exchange_probabilities()
+        return pd.DataFrame(self.df_repl_pr.describe().round(3))
+
+    def save(self, dir_out: Path = None):
+        """
+        Save class attributes to files.
+
+        Args:
+            dir_out (Path): Path to output directory. Defaults to None.
+        """
+        if dir_out is None:
+            dir_out = self.f_log.parents[1] / "analysis" / self.name
+
+        dir_out.mkdir(parents=True, exist_ok=True)
+
+        self.df_repl_pr.to_csv(dir_out / "exchange_probability_dynamics.csv")
+        self.df_repl_pr_summary.to_csv(dir_out / "exchange_probability_summary.csv")
+
+        f_out = dir_out / "exchange_probability_average.txt"
+        avgs = self.df_repl_pr.mean().round(3)
+        stds = self.df_repl_pr.std().round(3)
+        with open(f_out, "w", encoding="utf-8") as f:
+            f.write(f"Datetime: {datetime.datetime.now()}")
+            f.write(f"Log file: {self.f_log}\n\n")
+            f.write(f"Number of replicas: {self.n_replica}\n")
+            f.write(f"Initial time: {self.t_initial:.1f} ns\n")
+            f.write(f"Final time: {self.t_final:.1f} ns\n")
+            f.write(f"Simulation time: {self.t_final - self.t_initial:.1f} ns\n\n")
+            f.write("Average +- Std Dev Exchange Probabilities:\n")
+            for i in range(self.n_replica - 1):
+                f.write(f"{i}-{i+1}: {avgs[i]:.2f} {stds[i]:.2f}\n")
+            f.write("\nStatistics:\n")
+            f.write(self.df_repl_pr_summary.round(3).to_string())
+
 
 def find_logs(dr: Path) -> list[Path]:
     """
@@ -30,167 +250,42 @@ def find_logs(dr: Path) -> list[Path]:
     Returns:
         list: List of log files.
     """
-    log_files = sorted(
-        list(Path(dr).rglob("**/replica_00/nvt_hremd_*.log")))
-    log_files.extend(sorted(
-        list(Path(dr).rglob("**/replica_00/2-mdrun-output/nvt_hremd_*.log"))))
+    log_files = sorted(list(Path(dr).rglob("**/replica_00/prod_*.log")))
     return log_files
 
 
-def parse_probabilities(f_log: Path) -> pd.DataFrame:
+def main(verbose: bool = False) -> None:
     """
-    Load text file and parse exchange probabilities
+    Main function for parsing log files and extracting exchange probabilities.
 
     Args:
-        f_log (Path): Input gmx mdrun log file
+        verbose (bool, optional): Print output to terminal. Defaults to False.
 
     Returns:
-        pd.DataFrame: Exchange probabilities at each step
+        None
+
     """
 
-    # parse text file and extract exchange probabilities
-    text = f_log.read_text()
-    prob_txt = re.findall(r"(?<=Repl pr).*", text)
-    prob_flt = []
-    tf = []
-
-    # prepare data for numpy array
-    for i in range(len(prob_txt)):
-
-        # remove alphabetic characters
-        prob_txt[i] = re.sub(r"[a-zA-Z]", "", prob_txt[i])
-
-        # remove leading and trailing whitespace
-        prob_txt[i] = prob_txt[i].strip()
-
-        # convert whitespace to comma
-        prob_txt[i] = re.sub(r"\s+", ",", prob_txt[i])
-
-        # convert comma separated string to list of floats
-        lst_float = []
-        for val in prob_txt[i].split(","):
-            try:
-                x = float(val)
-                lst_float.append(x)
-            except ValueError:
-                warnings.warn(f"ValueError: {val}")
-                # lst_float.append(np.nan)
-
-        lst_zeros = [np.nan] * len(lst_float)
-
-        # place zero elements between non-zero elements
-        lst_complete = [x for l in list(
-            zip(lst_float, lst_zeros)) for x in l][:-1]
-
-        # prepend and append to list if necessary
-        if len(prob_flt) == 0:
-            pass
-        elif (len(lst_complete) != len(prob_flt[-1])):
-            lst_complete = [np.nan] + lst_complete + [np.nan]
-
-        # check that list is the same length as previous list
-        if len(prob_flt) > 0:
-            if len(lst_complete) != len(prob_flt[-1]):
-                warnings.warn(
-                    f"List length mismatch: {len(lst_complete)} vs {len(prob_flt[-1])}")
-                continue
-
-        # output results
-        prob_flt.append(lst_complete)
-
-    # calculate final simulation time
-    times = re.findall(r"(?<=time\s)\d+.\d+", text)
-    times = [float(x) for x in times]
-    try:
-        tf = max(times) / 1000.
-    except ValueError:
-        tf = np.nan
-
-    # convert to pandas dataframe
-    if len(prob_flt) > 0:
-        idxs = range(len(prob_flt[0]))
-        cols = [f"{i}-{i+1}" for i in idxs]
-        return pd.DataFrame(prob_flt, columns=cols), tf
-    else:
-        return pd.DataFrame(), tf
-
-
-plt.rcParams["axes.formatter.use_mathtext"] = True
-plt.rcParams["font.family"] = "serif"
-plt.rcParams["font.serif"] = "cmr10"
-
-
-if __name__ == "__main__":
-    # Input path
-    d_data = f"{Path.cwd()}/data"
+    # Input data path
+    d_data = Path(__file__).parents[1] / "data"
 
     # Find all log files in the current directory
     f_logs = find_logs(d_data)
-    print(f"Found {len(f_logs)} log files")
+    if verbose:
+        print(f"Found {len(f_logs)} log files in {d_data}")
 
     # Parse log files and extract exchange probabilities
-    for (idx, log_file) in enumerate(f_logs):
+    for idx, log_file in enumerate(f_logs):
         print(f"Reading log file {idx+1}/{len(f_logs)}: {log_file}")
-        df, t = parse_probabilities(log_file)
-
-        # calculate average exchange probability
-        avg = pd.DataFrame(df.mean(axis=0))
-        try:
-            stats = pd.DataFrame(df.describe())
-        except:
-            stats = pd.DataFrame()
-
-        # output probabilities to file
-        if "prod" in log_file.stem:
-            d_out = log_file.parents[1] / "analysis" / "replica_exchange"
-        else:
-            d_out = log_file.parents[1] / "analysis" / "replica_exchange"
-
-        d_out.mkdir(parents=True, exist_ok=True)
+        log = ParseGmxLog(log_file)
+        log.save()
 
         # output probabilities to terminal
-        print(f"Output path: {d_out}")
-        print(f"Average Exchange probabilities (tf = {t:.1f} ns):")
-        print(avg.to_string(float_format="{:.2f}".format))
-        print()
+        if verbose:
+            print("Average Exchange probabilities" + f" (tf = {log.t_final:.1f} ns):")
+            print(log.df_repl_pr_summary.to_string())
+            print()
 
-        f_out = d_out / "exchange_probability_dynamics.csv"
-        df.to_csv(f_out)
 
-        f_out = d_out / "exchange_probability_average.txt"
-        with open(f_out, "w", encoding="utf-8") as f:
-            # date and time
-            f.write(f"Datetime: {datetime.datetime.now()}")
-            f.write("\n\n")
-            # exchange probabilities
-            f.write(f"Average Exchange probabilities (tf: {t:.1f} ns):\n")
-            f.write(avg.round(decimals=2).to_string(header=False))
-            f.write("\n")
-            f.write(f"Average of averages: {avg.mean().values[0]:.2f}\n")
-            f.write(f"Standard deviation: {avg.std().values[0]:.2f}\n")
-            # whitespace
-            f.write("\n\n")
-            # statistics
-            f.write("Statistics:\n")
-            f.write(stats.round(decimals=2).to_string())
-
-        # convert probabilities to percentage
-        avg *= 100
-        avg = avg.round(0).astype(int)
-
-        # continue loop if no data
-        if len(avg) == 0:
-            continue
-
-        # output probabilities as table to file
-        f_out = d_out / "exchange_probability_average.png"
-        fig = plt.figure(figsize=(1.5, 7))
-        ax = fig.add_subplot(111, frame_on=False)
-        ax.axis("off")
-        ax.axis("tight")
-        ax.set_title(r"$t_f = $ " + f"{t:.1f} [ns]")
-        ax.table(cellText=avg.values, rowLabels=avg.index,
-                 colLabels=[r"$P(\mathrm{Exch.})$ [%]"],
-                 fontsize=32, loc="center")
-        fig.tight_layout()
-        fig.savefig(f_out, dpi=600)
+if __name__ == "__main__":
+    main(verbose=VERBOSE)
