49d6f730112a4febf9c9f30a55e5a5832c21d085
Alec <30010253+alec-glisman@users.noreply.github.com>
2024-01-19 08:13:02 -0800

    Refactor code to improve performance and readability

--- submission/multiple_submission_with_idx.sh
+++ submission/multiple_submission_with_idx.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-n_jobs='8'
+n_jobs='12'
 first_dependency='1'
 dependency_code='afterany'
 batch_script='2oneopes_6.sh'
@@ -16,10 +16,10 @@ fi
 job_name="OP_6.${idx}"
 
 # iterate over all jobs
-echo "Submitting ${n_jobs} jobs with script ${batch_script}"
+echo "Submitting ${n_jobs} jobs with script ${batch_script} ${idx}"
 jid_dep="${first_dependency}"
 for job in $(seq 1 "${n_jobs}"); do
-    jid="$(sbatch --dependency="${dependency_code}:${jid_dep}" --job-name "${job_name}-${job}" "${batch_script}" "${idx}" "${batch_script}" | awk '{print $4}')"
-    echo "Submitted job ${job}/${n_jobs} with job id ${jid} and dependency ${jid_dep}"
+    jid="$(sbatch --dependency="${dependency_code}:${jid_dep}" --job-name "${job_name}-${job}" "${batch_script}" "${idx}" | awk '{print $4}')"
+    echo "Submitted job ${job_name}-${job} (${job}/${n_jobs}) with job id ${jid} and dependency ${jid_dep}"
     jid_dep="${jid}"
 done
