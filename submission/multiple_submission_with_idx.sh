128d3275027aecf7782f99d9668c151166c4d5ab
Alec <30010253+alec-glisman@users.noreply.github.com>
2024-01-18 22:26:23 -0800

    starting production simulations

--- submission/multiple_submission_with_idx.sh
+++ submission/multiple_submission_with_idx.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-n_jobs='3'
+n_jobs='8'
 first_dependency='1'
 dependency_code='afterany'
-batch_script='1eqbm_6.sh'
+batch_script='2oneopes_6.sh'
 
 # argument parsing
 # 1st argument: input global indices to run {0..19}
@@ -13,13 +13,13 @@ if [ $# -eq 0 ]; then
 else
     idx="${1}"
 fi
-job_name="Eqbm6-${idx}"
+job_name="OP_6.${idx}"
 
 # iterate over all jobs
 echo "Submitting ${n_jobs} jobs with script ${batch_script}"
 jid_dep="${first_dependency}"
 for job in $(seq 1 "${n_jobs}"); do
-    jid="$(sbatch --dependency="${dependency_code}:${jid_dep}" --job-name "${job_name}" "${batch_script}" "${idx}" "${batch_script}" | awk '{print $4}')"
+    jid="$(sbatch --dependency="${dependency_code}:${jid_dep}" --job-name "${job_name}-${job}" "${batch_script}" "${idx}" "${batch_script}" | awk '{print $4}')"
     echo "Submitted job ${job}/${n_jobs} with job id ${jid} and dependency ${jid_dep}"
     jid_dep="${jid}"
 done
