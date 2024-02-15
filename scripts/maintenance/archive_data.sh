62aaf856bc62f9246d77316c2b6753cfca171b5e
Alec <30010253+alec-glisman@users.noreply.github.com>
2024-02-14 16:46:01 -0800

    Add script for archiving and moving data

---
+++ scripts/maintenance/archive_data.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+# Created by Alec Glisman (GitHub: @alec-glisman) on February 2nd, 2023
+#
+
+# built-in shell options
+set -o errexit # exit when a command fails. Add || true to commands allowed to fail
+set -o nounset # exit when script tries to use undeclared variables
+
+# data I/O directories
+input_dir_base='/nfs/zeal_nas/home_mount/aglisman/GitHub/Polyelectrolyte-Surface-Adsorption/data_archive'
+output_dir_base='/nfs/zeal_nas/data_mount/aglisman-data/1-electronic-continuum-correction/6-surface-study-test-calculations'
+
+# ########################################################################## #
+# Find and delete files                                                      #
+# ########################################################################## #
+find_and_delete_files() {
+    # args
+    local file_type=$1
+    local patterns=("${@:2}")
+
+    # find files
+    echo "INFO) Finding ${file_type} files"
+    local files
+    files="$(find "${input_dir_base}" -type f -name "*${file_type}")"
+    mapfile -t files <<<"${files}"
+
+    # exclude files containing excluded patterns
+    local len="${#files[@]}"
+    for pattern in "${patterns[@]}"; do
+        files=("${files[@]/${pattern}/}")
+    done
+    for i in "${!files[@]}"; do
+        if [[ -z "${files[i]}" ]]; then
+            unset 'files[i]'
+        fi
+    done
+    echo "DEBUG) Removed $((len - ${#files[@]})) ${file_type} files for containing excluded patterns"
+
+    # list files to be deleted
+    if [[ "${#files[@]}" -eq 0 ]]; then
+        echo "WARNING) No ${file_type} files found"
+        return
+    fi
+    echo "INFO) Here are the ${file_type} files to be deleted:"
+    for file in "${files[@]}"; do
+        echo "${file}"
+    done
+
+    # prompt user to delete files
+    read -p "Do you want to delete these files? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        echo "INFO) Deleting ${file_type} files"
+        parallel --keep-order --ungroup --jobs '32' --eta --halt-on-error '2' \
+            rm --verbose "{1}" \
+            ::: "${files[@]}"
+    else
+        echo "INFO) Not deleting ${file_type} files"
+    fi
+}
+
+# Backup files
+find_and_delete_files '#*#' '*log*'
+# TRR files
+find_and_delete_files '.trr' '*/replica_00/*' '*/3-sampling-md/*' '*/3-sampling-metad/*'
+# XTC files
+find_and_delete_files '.xtc' '*/3-sampling-*/*'
+
+# ########################################################################## #
+# Move data to new directory with rsync                                      #
+# ########################################################################## #
+# move files
+echo "INFO) Moving files to new directory"
+
+# prompt user to accept or reject moving files
+echo "DEBUG) Source directory: ${input_dir_base}"
+echo "DEBUG) Destination directory: ${output_dir_base}"
+read -p "Do you want to move files from the source to the destination directory? (y/n) " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo "INFO) Not moving files"
+    exit 0
+fi
+
+# move files
+echo "INFO) Moving files"
+rsync --archive --verbose --progress --human-readable --remove-source-files \
+    "${input_dir_base}/" "${output_dir_base}/"
+
+echo "INFO) Done"
