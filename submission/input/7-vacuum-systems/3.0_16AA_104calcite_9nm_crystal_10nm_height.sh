7bd6f25ac503e518af0cfe2233e82171d14da5ba
Alec <30010253+alec-glisman@users.noreply.github.com>
2024-03-05 21:16:15 -0800

    all simulations in 7 with 2 slabs

--- submission/input/7-vacuum-systems/3.0_16AA_104calcite_9nm_crystal_10nm_height.sh
+++ submission/input/7-vacuum-systems/3.0_16AA_104calcite_9nm_crystal_10nm_height.sh
@@ -42,7 +42,7 @@ export PDB_BULK_ZMIN='4.62' # z-coordinate of bottom of bulk part of crystal in 
 export PDB_BULK_ZMAX='5.09' # z-coordinate of top of bulk part of crystal in nm in PDB file
 
 # number of each component
-export N_SLAB='1'      # number of crystal slabs {1, 2}
+export N_SLAB='2'      # number of crystal slabs {1, 2}
 export N_MONOMER='1'   # number of monomers in chain {1, 2, 5, 8, 16, 32}
 export N_CHAIN='16'    # number of chains
 export N_CARBONATE='0' # number of aqueous carbonate ions
