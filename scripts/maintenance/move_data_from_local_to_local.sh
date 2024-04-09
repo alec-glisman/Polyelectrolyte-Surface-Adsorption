980fa702011ceb5783c3ca153d2841a6d3a25871
Alec <30010253+alec-glisman@users.noreply.github.com>
2024-04-08 20:46:10 -0700

    Add script for moving data from local to local directory

--- scripts/maintenance/move_data_from_local_to_local.sh
+++ scripts/maintenance/move_data_from_local_to_local.sh
@@ -8,10 +8,8 @@ set -o errexit # exit when a command fails. Add || true to commands allowed to f
 set -o nounset # exit when script tries to use undeclared variables
 
 # data I/O directories
-remote_address='aglisman@zeal.caltech.edu'
-remote_dir='/nfs/zeal_nas/home_mount/aglisman/GitHub/Polyelectrolyte-Surface-Adsorption/data_archive/6_single_chain_binding/cleaned'
-local_dir='/Volumes/ExFat/single_chain_binding'
-pattern='*/2-concatenated/*'
+source_dir='/nfs/zeal_nas/home_mount/aglisman/GitHub/Polyelectrolyte-Surface-Adsorption/data_archive/6_single_chain_binding/cleaned'
+dest_dir='/nfs/zeal_nas/data_mount/aglisman-data/1-electronic-continuum-correction/7-single-chain-surface-binding'
 
 # ########################################################################## #
 # Move data to new directory with rsync                                      #
@@ -19,17 +17,16 @@ pattern='*/2-concatenated/*'
 
 # prompt user to accept or reject moving files
 echo "INFO) Moving files to new directory"
-echo "DEBUG) Source directory: ${remote_address}:${remote_dir}"
-echo "DEBUG) Destination directory: ${local_dir}"
+echo "DEBUG) Source directory: ${source_dir}"
+echo "DEBUG) Destination directory: ${dest_dir}"
 read -p "Do you want to move files from the source to the destination directory? (y/n) " -n 1 -r
 echo
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     echo "INFO) Not moving files"
 else
     echo "INFO) Moving files"
-    rsync --archive --verbose --progress --human-readable --dry-run \
-    --include="${pattern}" --exclude="*" \
-    "${remote_address}:${remote_dir}/" "${local_dir}/"
+    rsync --verbose --archive --progress --human-readable --relative --stats \
+    "${source_dir}/." "${dest_dir}/"
 fi
 
 echo "INFO) Done"
---
+++ scripts/maintenance/move_data_from_remote_to_local.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+# Created by Alec Glisman (GitHub: @alec-glisman) on February 2nd, 2023
+#
+
+# built-in shell options
+set -o errexit # exit when a command fails. Add || true to commands allowed to fail
+set -o nounset # exit when script tries to use undeclared variables
+
+# data I/O directories
+remote_address='aglisman@zeal.caltech.edu'
+remote_dir='/nfs/zeal_nas/home_mount/aglisman/GitHub/Polyelectrolyte-Surface-Adsorption/data_archive/6_single_chain_binding/cleaned'
+local_dir='/Users/alecglisman/Downloads/single_chain_binding' # '/Volumes/ExFat/single_chain_binding'
+pattern='*/3-sampling-opes-one/replica_00/2-concatenated/*'
+
+# ########################################################################## #
+# Move data to new directory with rsync                                      #
+# ########################################################################## #
+
+# prompt user to accept or reject moving files
+echo "INFO) Moving files to new directory"
+echo "DEBUG) Source directory: ${remote_address}:${remote_dir}"
+echo "DEBUG) Destination directory: ${local_dir}"
+read -p "Do you want to move files from the source to the destination directory? (y/n) " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo "INFO) Not moving files"
+else
+    echo "INFO) Moving files"
+    rsync --verbose --archive --progress --human-readable --relative --stats \
+    "${remote_address}:${remote_dir}/./${pattern}" "${local_dir}/"
+fi
+
+echo "INFO) Done"
