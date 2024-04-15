e472c37342d1bd8a888c9cd6d6f61ba200f7aa0f
Alec <30010253+alec-glisman@users.noreply.github.com>
2024-04-15 10:15:32 -0700

    added files from Decai

--- scripts/maintenance/move_data_from_remote_to_local.sh
+++ scripts/maintenance/move_data_from_remote_to_local.sh
@@ -10,25 +10,45 @@ set -o nounset # exit when script tries to use undeclared variables
 # data I/O directories
 remote_address='aglisman@zeal.caltech.edu'
 remote_dir='/nfs/zeal_nas/home_mount/aglisman/GitHub/Polyelectrolyte-Surface-Adsorption/data_archive/6_single_chain_binding/cleaned'
-local_dir='/Users/alecglisman/Downloads/single_chain_binding' # '/Volumes/ExFat/single_chain_binding'
-pattern='*/3-sampling-opes-one/replica_00/2-concatenated/*'
+local_dir='/media/aglisman/Data/Single-Chain-Adsorption'
+patterns=('3-sampling-opes-one/replica_00/2-concatenated' '3-sampling-opes-one/replica_00/4-polymer' '3-sampling-opes-one/analysis/combined')
 
 # ########################################################################## #
 # Move data to new directory with rsync                                      #
 # ########################################################################## #
+echo "INFO) Moving files to new directory"
+echo "DEBUG) Destination directory: ${local_dir}"
+
+# find all directories on the remote server that match the pattern
+echo "INFO) Listing files in source directory"
+remote_files=""
+for pattern in "${patterns[@]}"; do
+    echo "DEBUG) Searching for files matching pattern: ${pattern}"
+    files=$(ssh "${remote_address}" 'find /nfs/zeal_nas/home_mount/aglisman/GitHub/Polyelectrolyte-Surface-Adsorption/data_archive/6_single_chain_binding/cleaned -type f' | grep "${pattern}")
+    remote_files+="${files}"
+done
+
+remote_files=$(echo "${remote_files}" | grep -v '\.lock' | grep -v '\.npz')
+remote_files=$(echo "${remote_files}" | sort | uniq)
+
+# remove remote dir path from remote files
+remote_files=$(echo "${remote_files}" | sed "s|${remote_dir}/||g")
+mapfile -t remote_files <<<"${remote_files}"
+
+for file in "${remote_files[@]}"; do
+    echo "DEBUG) ${file}"
+done
 
 # prompt user to accept or reject moving files
-echo "INFO) Moving files to new directory"
-echo "DEBUG) Source directory: ${remote_address}:${remote_dir}"
-echo "DEBUG) Destination directory: ${local_dir}"
 read -p "Do you want to move files from the source to the destination directory? (y/n) " -n 1 -r
 echo
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     echo "INFO) Not moving files"
 else
-    echo "INFO) Moving files"
-    rsync --verbose --archive --progress --human-readable --relative --stats \
-    "${remote_address}:${remote_dir}/./${pattern}" "${local_dir}/"
+    echo "INFO) Moving files in parallel"
+    parallel --jobs 24 --bar --halt-on-error '0' --joblog 'move_data_from_remote_to_local.log' \
+        rsync --archive --progress --verbose --human-readable --mkpath "${remote_address}:${remote_dir}/{1}" "${local_dir}/{1}" \
+        ::: "${remote_files[@]}"
 fi
 
 echo "INFO) Done"
